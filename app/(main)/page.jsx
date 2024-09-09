12d26e343f48abbcec68b968eef60bc7ad20fcdb
munadifathullah123 <munadifathullah123@gmail.com>
2024-09-09 06:48:07 +0000

    feat:oke

--- app/(main)/page.jsx
+++ app/(main)/page.jsx
@@ -18,7 +18,7 @@ export const metadata = {
 export default async function Home() {
   
  
-  const supabase = await createClient('')
+  const supabase =  createClient('')
   const { data, error } = await supabase.from('articles').select(`*,detail_user(slug,users(name,image))`).order('created_at', { ascending: false });
   console.log({error})
   // Fungsi untuk mendapatkan paragraf deskripsi
