9a6d00f7499e4b7453d24617471f3f466281a156
munadifathullah123 <munadifathullah123@gmail.com>
2024-09-09 07:54:01 +0000

    feat:oke

---
+++ middleware.js
@@ -0,0 +1,12 @@
+import { withAuth } from "next-auth/middleware"
+
+export default withAuth({
+  // Matches the pages config in `[...nextauth]`
+  pages: {
+    signIn: "/",
+    error: "/error",
+  },
+})
+export const config = {
+  matcher: ['/dashboard(.*)',],  // Jalur yang dilindungi
+}
