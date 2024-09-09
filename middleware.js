63f9b95752cdca4bd14433b13db65b95d8d486fc
munadifathullah123 <munadifathullah123@gmail.com>
2024-09-09 07:14:08 +0000

    feat:oke

--- middleware.js
+++ middleware.js
@@ -8,5 +8,5 @@ export default withAuth({
   },
 })
 export const config = {
-  matcher: ['/(dashboard)/dashboard'],  // Jalur yang dilindungi
+  matcher: ['/dashboard'],  // Jalur yang dilindungi
 }
